d42ad33e7dc177111e5410aa9b35b3d7058f3ec1
janrt-dev <jaja.abdelkarim@inpt.ac.ma>
2023-04-06 11:12:53 +0000

    I have added the caching option for preventing expensive operations

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,12 @@ Route::get('/', function () {
 
 
 Route::get('posts/{post}', function($slug){
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if(! file_exists($path)){
+    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
         return redirect('/');
     }
-    $post = file_get_contents($path);
-    return view('post',[
-        'post' => $post
-    ]);
+
+    $post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
+
+    return view('post',['post' => $post]);
 })->where('post', '[a-zA-Z_\-]+');
