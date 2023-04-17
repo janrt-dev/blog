0dccf5d5b80ae0f6267dc3e12b424d6a4409839d
janrt-dev <jaja.abdelkarim@inpt.ac.ma>
2023-04-17 11:33:39 +0000

    add the layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="s/app.js"></script>
-</head>
-<body>
+
+@extends('layout')
+@section('content')
     <article>
 
         {!!$post->body  !!}
     </article>
     <a href="/">Go Back</a>
-</body>
-</html>
+
+@endsection
+
+
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<jjjkhtml lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="s/app.js"></script>
-</head>
-<body>
+@extends('layout')
+@section('content')
+
 @foreach ($posts as $post)
   <article>
    {{--  {!!   $post!!}  --}}
@@ -20,5 +12,4 @@
   <p> {!!$post->excerpt  !!}</p>
     </article>
 @endforeach
-<body>
-</html>
+@endsection
